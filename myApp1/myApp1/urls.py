40676e9a1f2277bdf72ca58ecceb1f22f0d49df9
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-09-10 20:01:23 +0530

    views and urls

--- myApp1/myApp1/urls.py
+++ myApp1/myApp1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myApp1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about-us/',views.aboutUS),
+    path('courses/', views.courses),
 ]
---
+++ myApp1/myApp1/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse 
+#browser pe render karane ke liye only text content
+
+def aboutUS(request):
+  return HttpResponse("<h1>This is about us page</h1>")
+
+def courses(request):
+  return HttpResponse("<h1>This is avaliable courses</h1>")
